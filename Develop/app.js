d6cf70b352bc6bc9a0eedb46991b580ec979cc65
Dan Cornutt <dacornutt@gmail.com>
2020-11-12 17:15:05 -0800

    App functioning correctly, done.

--- Develop/app.js
+++ Develop/app.js
@@ -12,29 +12,6 @@ const render = require("./lib/htmlRenderer");
 const { exit } = require("process");
 const { resolve } = require("path");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
 const qLib = {
     name: {
         type: "input",
@@ -89,6 +66,9 @@ const personLib = {
 //Global team array of people objects
 let team = [];
 
+const outputFolder = "./output";
+const outputFile = "team.html";
+
 //Builds string array of questions to ask based on parameter value, adds teammember object to team array
 //Params: string of person to be added to team
 async function addToTeam(person) {
@@ -108,12 +88,10 @@ function askQuestions(questions) {
 }
 
 function writeHTML(htmlData) {
-    fs.access('./output'), function(err) {
-        if (err && err.code === 'ENOENT') {
-          fs.mkdir(path.join(__dirname, 'output'), err => console.log(err));
-        }
-    };
-    fs.writeFile("./", htmlData, function(err) {
+    if (!fs.existsSync(OUTPUT_DIR)){
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+    fs.writeFile(outputPath, htmlData, function(err) {
         if (err) {
             return console.log(err)
         }
@@ -137,7 +115,6 @@ async function init() {
             buildingTeam = false;
         }
     }
-    console.log(team);
     writeHTML(render(team));
 };
 
